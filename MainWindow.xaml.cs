0ac953ea2fd5532a27cbce361fa053e381c638ae
Doug pace <34288598+whirly1227@users.noreply.github.com>
2024-02-15 16:53:29 -0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,191 @@
+﻿using Gma.System.MouseKeyHook;
+using Helldivers2_Hotkeys.Properties;
+using System.Configuration;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Keyboard = InputMethods.Keyboard;
+
+namespace Helldivers2_Hotkeys
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal static List<string> StratList = typeof(Stratagems).GetFields()
+                            .Select(field => field.Name.Replace("_", " ")).ToList();
+        private IKeyboardMouseEvents m_GlobalHook = Hook.GlobalEvents();
+        private Stratagems stratagems = new();
+
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            StratList.Sort();
+            InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                ComboBox comboBox = new()
+                {
+                    Name = "Combo_" + i,
+                    Margin = new Thickness(0, 2, 2, 2),
+                    FontFamily = new FontFamily("Arial"),
+                    ItemsSource = StratList,
+                    VerticalContentAlignment = VerticalAlignment.Center,
+                    HorizontalContentAlignment = HorizontalAlignment.Left
+                };
+                Grid.SetRow(comboBox, i);
+                Grid.SetColumn(comboBox, 1);
+                MainGrid.Children.Add(comboBox);
+            }
+            LoadSettings();
+        }
+
+        private void BTN_Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveSettings();
+        }
+        
+        private void BTN_StartStop_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (BTN_StartStop.Content.ToString() == "Start")
+                {
+                    m_GlobalHook.KeyUp += M_GlobalHook_KeyPress;
+                    BTN_StartStop.Content = "Stop";
+                }
+                else
+                {
+                    m_GlobalHook.KeyUp -= M_GlobalHook_KeyPress;
+                    BTN_StartStop.Content = "Start";
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void LoadSettings()
+        {
+            try
+            {
+                Settings.Default.Numpad0 = Settings.Default.Numpad0;
+                Settings.Default.Numpad1 = Settings.Default.Numpad1;
+                Settings.Default.Numpad2 = Settings.Default.Numpad2;
+                foreach (SettingsPropertyValue prop in Settings.Default.PropertyValues)
+                {
+                    prop.Name.ToString();
+                    foreach (object? item in MainGrid.Children)
+                    {
+                        if (item.GetType() == typeof(ComboBox))
+                        {
+                            ComboBox comboBox = (ComboBox)item;
+                            if (comboBox.Name.Replace("Combo_", "") == prop.Name.Replace("Numpad", ""))
+                            {
+                                comboBox.Text = prop.PropertyValue.ToString().Replace("_", " ");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void SaveSettings()
+        {
+            try
+            {
+                foreach (SettingsPropertyValue prop in Settings.Default.PropertyValues)
+                {
+                    prop.Name.ToString();
+                    foreach (object? item in MainGrid.Children)
+                    {
+                        if (item.GetType() == typeof(ComboBox))
+                        {
+                            ComboBox comboBox = (ComboBox)item;
+                            if (comboBox.Name.Replace("Combo_", "") == prop.Name.Replace("Numpad", ""))
+                            {
+                                if (comboBox.SelectedIndex < 0) break;
+                                prop.PropertyValue = comboBox.Text.Replace(" ", "_");
+                            }
+                        }
+                    }
+                }
+                Settings.Default.Save();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void M_GlobalHook_KeyPress(object? sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case System.Windows.Forms.Keys.NumPad0:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad0).GetValue(stratagems) as Key[]);
+                    break;
+                case System.Windows.Forms.Keys.NumPad1:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad1).GetValue(stratagems) as Key[]);
+                    break;
+                case System.Windows.Forms.Keys.NumPad2:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad2).GetValue(stratagems) as Key[]);
+                    break;
+                case System.Windows.Forms.Keys.NumPad3:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad3).GetValue(stratagems) as Key[]);
+                    break;
+                case System.Windows.Forms.Keys.NumPad4:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad4).GetValue(stratagems) as Key[]);
+                    break;
+                case System.Windows.Forms.Keys.NumPad5:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad5).GetValue(stratagems) as Key[]);
+                    break;
+                case System.Windows.Forms.Keys.NumPad6:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad6).GetValue(stratagems) as Key[]);
+                    break;
+                case System.Windows.Forms.Keys.NumPad7:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad7).GetValue(stratagems) as Key[]);
+                    break;
+                case System.Windows.Forms.Keys.NumPad8:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad8).GetValue(stratagems) as Key[]);
+                    break;
+                case System.Windows.Forms.Keys.NumPad9:
+                    DoStrat(stratagems.GetType().GetField(Settings.Default.Numpad9).GetValue(stratagems) as Key[]);
+                    break;
+            }
+        }
+
+        private void DoStrat(Key[] strat)
+        {
+            Keyboard.Press(Key.LeftCtrl);
+            Thread.Sleep(50);
+            for (int i = 0; i < strat.Length; i++)
+            {
+                Keyboard.Press(strat[i]);
+                Thread.Sleep(25);
+                Keyboard.Release(strat[i]);
+                Thread.Sleep(25);
+            }
+            Keyboard.Release(Key.LeftCtrl);
+            Thread.Sleep(50);
+        }
+    }
+}
---
+++ Stratagems.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Keyboard = InputMethods.Keyboard;
+
+namespace Helldivers2_Hotkeys
+{
+    internal class Stratagems
+    {
+        private static Key Up = Key.W;
+        private static Key Down = Key.S;
+        private static Key Left = Key.A;
+        private static Key Right = Key.D;
+
+        public static Key[] Resupply                       = [Down, Down, Up, Right];
+        public static Key[] Reinforce                      = [Up, Down, Right, Left, Up];
+        public static Key[] SOS                            = [Up, Down, Right, Up];
+        public static Key[] Machine_Gun                    = [Down, Left, Down, Up, Right];
+        public static Key[] Anti_Materiel_Rifle            = [Down, Left, Right, Up, Down];
+        public static Key[] Stalwart                       = [Down, Left, Down, Up, Up, Left];
+        public static Key[] Expendable_Anti_Tank           = [Down, Down, Left, Up, Right];
+        public static Key[] Recoilless_Rifle	           = [Down, Left, Right, Right, Left];
+        public static Key[] Flamethrower                   = [];
+        public static Key[] Autocannon                     = [Down, Left, Down, Up, Up, Right];
+        public static Key[] Railgun                        = [];
+        public static Key[] Spear                          = [];
+        public static Key[] Eagle_Strafing_Run             = [Up, Right, Right];
+        public static Key[] Eagle_Airstrike	               = [Up, Right, Down, Right];
+        public static Key[] Eagle_Cluster_Bomb             = [Up, Right, Down, Down, Right];
+        public static Key[] Eagle_Napalm_Airstrike         = [Up, Right, Down, Up];
+        public static Key[] Eagle_Smoke_Strike             = [];
+        public static Key[] Eagle_110MM_Rocket_Pods        = [Up, Right, Up, Left];
+        public static Key[] Eagle_500KG_Bomb               = [Up, Right, Down, Down, Down];
+        public static Key[] Orbital_Gatling_Barrage        = [Right, Down, Left, Up, Up];
+        public static Key[] Orbital_Airburst_Strike        = [Right, Right, Right];
+        public static Key[] Orbital_120MM_HE_Barrage	   = [Right, Right, Down, Left, Right, Down];
+        public static Key[] Orbital_380MM_HE_Barrage       = [Right, Down, Up, Up, Left, Down, Down];
+        public static Key[] Orbital_Walking_Barrage        = [];
+        public static Key[] Orbital_Laser	               = [];
+        public static Key[] Orbital_Railcannon_Strike      = [];
+        public static Key[] Orbital_Precision_Strike       = [Right, Right, Up];
+        public static Key[] Orbital_Gas_Strike             = [];
+        public static Key[] Orbital_EMS_Strike             = [];
+        public static Key[] Orbital_Smoke_Strike           = [];
+        public static Key[] Jump_Pack                      = [Down, Up, Up, Down, Up];
+        public static Key[] HMG_Replacement                = [];
+        public static Key[] Shield_Generator_Relay         = [];
+        public static Key[] Tesla_Tower                    = [];
+        public static Key[] Anti_Personnel_Minefield       = [];
+        public static Key[] Supply_Pack                    = [];
+        public static Key[] Grenade_Launcher               = [];
+        public static Key[] Laser_Canon                    = [];
+        public static Key[] Incendiary_Mines               = [];
+        public static Key[] Guard_Dog_Rover                = [];
+        public static Key[] Ballistic_Shield_Backpack      = [];
+        public static Key[] Arc_Thrower                    = [];
+        public static Key[] Shield_Generator_Pack          = [Down, Up, Left, Right, Left, Right];
+        public static Key[] Machine_Gun_Sentry             = [Down, Up, Right, Right, Up];
+        public static Key[] Gatling_Sentry                 = [Down, Up, Right, Left];
+        public static Key[] Mortar_Sentry                  = [];
+        public static Key[] Guard_Dog                      = [Down, Up, Left, Up, Right, Down];
+        public static Key[] Autocannon_Sentry	           = [];
+        public static Key[] Rocket_Sentry	               = [];
+        public static Key[] EMS_Mortar_Sentry              = [];
+    }
+}
